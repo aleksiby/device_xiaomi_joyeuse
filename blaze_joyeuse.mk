b997637af17954b06663bccad52a627155645dc2
aleksiby <nekoskuy.ubuntu@gmail.com>
2022-07-05 09:59:13 +0700

    joyeuse: Adapt to Project Blaze
    Signed-off-by: aleksiby <nekoskuy.ubuntu@gmail.com>

--- blaze_joyeuse.mk
+++ blaze_joyeuse.mk
@@ -15,13 +15,13 @@
 # Inherit from joyeuse device tree
 $(call inherit-product, device/xiaomi/joyeuse/device.mk)
 
-# Inherit some common from AOSP ROM stuff
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common from blaze ROM stuff
+$(call inherit-product, vendor/blaze/config/common_full_phone.mk)
 
 # Bootanimation Resolution
 TARGET_BOOT_ANIMATION_RES := 1080
 
-PRODUCT_NAME := aosp_joyeuse
+PRODUCT_NAME := blaze_joyeuse
 PRODUCT_DEVICE := joyeuse
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 9 Pro
@@ -33,3 +33,14 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="raven-user 12 S2B3.220205.007.A1 8133516 release-keys"
 
 BUILD_FINGERPRINT := google/raven/raven:12/S2B3.220205.007.A1/8133516:user/release-keys
+
+# Blaze Official Stuff
+BLAZE_BUILD_TYPE := Maverick
+BLAZE_MAINTAINER := Alchemist-Dev
+TARGET_USE_PIXEL_CHARGER := false
+TARGET_SUPPORTS_NEXT_GEN_ASSISTANT := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+WITH_GAPPS := true
+TARGET_GAPPS_ARCH := arm64
+IS_PHONE := true
